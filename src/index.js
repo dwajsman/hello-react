5318be73a03b2605dc5f0558dee68e8e5e41eb8a
Daniel Wajsman <danielwajsman@mac.com>
2021-12-07 00:24:03 -0800

    working version

--- src/index.js
+++ src/index.js
@@ -4,21 +4,33 @@ import './index.css';
 
 const Button = (props) => {
   // your code here
+  const {reset} = props;
+
+  return (
+    <button onClick={reset}> Reset </button>
+  );
 };
 
 const Application = () => {
-
+  const [word, setWord] = useState("");
   // your code here
-
+  
   const reset = () => {
-    console.log("reset");
+    // console.log("reset");
     // your code here
+    setWord("");
   };
 
   return (
     <main>
       {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input
+        value={word}
+        onChange={(event) => setWord(event.target.value)}
+        placeholder="Type your name"
+      />
+      <Button reset={reset} />
+      <h1>Hello {word}</h1>
     </main>
   );
 };
